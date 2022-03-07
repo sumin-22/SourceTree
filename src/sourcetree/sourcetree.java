de8fd297b26863f332279d54005798d33c5f75d6
sumin-22 <100885815+sumin-22@users.noreply.github.com>
2022-03-07 22:20:28 +0900

    Update sourcetree.java

--- src/sourcetree/sourcetree.java
+++ src/sourcetree/sourcetree.java
@@ -5,6 +5,7 @@ public class sourcetree {
 	public static void main(String[] args) {
 		System.out.println("Hello :0");
 		System.out.println("Bye Bye~");	
+		System.out.println("Hey Hey!");
 		
 	}
 
